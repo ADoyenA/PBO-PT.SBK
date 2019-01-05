78818814cb4b0de5fc39ee1e5448804899f5fa5d
Muhammad Walidin <mawalidin@gmail.com>
2019-01-06 02:02:30 +0700

    Tab Slider untk Home,Barang,About
    for tugas besar PBO Android PT.SBK

---
+++ RecyclerView/app/src/main/java/com/demotxt/myapp/recyclerview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.demotxt.myapp.recyclerview;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // Add Fragment Here
+
+        adapter.AddFragment(new FragmentHome(),"");
+        adapter.AddFragment(new FragmentBarang(),"");
+        adapter.AddFragment(new FragmentCreator(),"");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_shopping);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_group);
+
+
+    }
+}
